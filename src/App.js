12b54adb0cd19bc838a009cb6b6e5d04ed286230
maxfle <maxecf@gmail.com>
2020-09-17 21:34:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,14 @@ class App extends React.Component {
     render() {
         const { advice } = this.state;
         return (
-            <h1>{advice}</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>Give me advice!</span>
+                    </button>
+                </div>
+            </div>
         );
     }
 }
